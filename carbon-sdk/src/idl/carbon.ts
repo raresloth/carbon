be3e03c12cd8f7be06b121c142bfb1343b18fc49
Austin Borden <austin.borden@gmail.com>
2023-03-27 12:11:17 +0700

    exported version

--- carbon-sdk/src/idl/carbon.ts
+++ carbon-sdk/src/idl/carbon.ts
@@ -1,5 +1,5 @@
 export type Carbon = {
-  "version": "0.1.0",
+  "version": "0.2.0",
   "name": "carbon",
   "instructions": [
     {
@@ -1442,7 +1442,7 @@ export type Carbon = {
 };
 
 export const IDL: Carbon = {
-  "version": "0.1.0",
+  "version": "0.2.0",
   "name": "carbon",
   "instructions": [
     {
