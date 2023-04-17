f71a40e08737ed7d5b679baeed20d7d4d888581b
Austin Borden <austin.borden@gmail.com>
2023-04-17 14:27:27 +0700

    optionally provide recentBlockhash to transaction helpers

--- carbon-sdk/src/transactions.ts
+++ carbon-sdk/src/transactions.ts
@@ -6,7 +6,7 @@ import { BuyVirtualArgs, ListVirtualArgs, MintVirtualArgs } from "./instructions
 export class Transactions {
 	constructor(public carbon: Carbon) {}
 
-	async listVirtual(args: ListVirtualArgs): Promise<Transaction> {
+	async listVirtual(args: ListVirtualArgs, recentBlockhash?: string): Promise<Transaction> {
 		const { seller, itemId, collectionMint, price, expiry, currencyMint } = args;
 
 		const tx = new Transaction();
@@ -22,12 +22,15 @@ export class Transactions {
 
 		tx.add(listIx);
 
-		await this.populateBlockhashAndFeePayer(tx);
+		await this.populateBlockhashAndFeePayer(tx, recentBlockhash);
 
 		return await this.carbon.provider.wallet.signTransaction(tx);
 	}
 
-	async buyVirtual(args: BuyVirtualArgs): Promise<{ mint: Keypair; transaction: Transaction }> {
+	async buyVirtual(
+		args: BuyVirtualArgs,
+		recentBlockhash?: string
+	): Promise<{ mint: Keypair; transaction: Transaction }> {
 		const { buyer, collectionConfig, listing, metadata, maxPrice } = args;
 
 		const tx = new Transaction().add(
@@ -46,7 +49,7 @@ export class Transactions {
 
 		tx.add(buyVirtualIxInfo.instruction);
 
-		await this.populateBlockhashAndFeePayer(tx);
+		await this.populateBlockhashAndFeePayer(tx, recentBlockhash);
 
 		let signedTx = await this.carbon.provider.wallet.signTransaction(tx);
 		signedTx.partialSign(buyVirtualIxInfo.mint);
@@ -58,7 +61,8 @@ export class Transactions {
 	}
 
 	async buyVirtualAndCustody(
-		args: BuyVirtualArgs
+		args: BuyVirtualArgs,
+		recentBlockhash?: string
 	): Promise<{ mint: Keypair; transaction: Transaction }> {
 		const { buyer, collectionConfig, listing, metadata, maxPrice } = args;
 
@@ -86,7 +90,7 @@ export class Transactions {
 
 		tx.add(custodyIx);
 
-		await this.populateBlockhashAndFeePayer(tx);
+		await this.populateBlockhashAndFeePayer(tx, recentBlockhash);
 
 		let signedTx = await this.carbon.provider.wallet.signTransaction(tx);
 		signedTx.partialSign(buyVirtualIxInfo.mint);
@@ -97,7 +101,10 @@ export class Transactions {
 		};
 	}
 
-	async mintVirtual(args: MintVirtualArgs): Promise<{ mint: Keypair; transaction: Transaction }> {
+	async mintVirtual(
+		args: MintVirtualArgs,
+		recentBlockhash?: string
+	): Promise<{ mint: Keypair; transaction: Transaction }> {
 		const tx = new Transaction().add(
 			ComputeBudgetProgram.setComputeUnitLimit({
 				units: 300_000,
@@ -107,7 +114,7 @@ export class Transactions {
 		const mintVirtualIxInfo = await this.carbon.instructions.mintVirtual(args);
 		tx.add(mintVirtualIxInfo.instruction);
 
-		await this.populateBlockhashAndFeePayer(tx);
+		await this.populateBlockhashAndFeePayer(tx, recentBlockhash);
 
 		let signedTx = await this.carbon.provider.wallet.signTransaction(tx);
 		signedTx.partialSign(mintVirtualIxInfo.mint);
@@ -118,9 +125,10 @@ export class Transactions {
 		};
 	}
 
-	async populateBlockhashAndFeePayer(tx: Transaction) {
+	async populateBlockhashAndFeePayer(tx: Transaction, recentBlockhash?: string) {
 		const provider: AnchorProvider = this.carbon.provider;
-		tx.recentBlockhash = (await provider.connection.getLatestBlockhash()).blockhash;
+		tx.recentBlockhash =
+			recentBlockhash ?? (await provider.connection.getLatestBlockhash()).blockhash;
 		tx.feePayer = provider.wallet.publicKey;
 	}
 }
